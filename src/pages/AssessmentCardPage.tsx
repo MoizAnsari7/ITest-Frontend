0d46b1cbf0d0d359221a34ec9b2a6ed60b601bc3
MoizAnsari7 <moizansari176@gmail.com>
2024-12-22 22:59:54 +0530

    Assesment Card Page Added

---
+++ src/pages/AssessmentCardPage.tsx
@@ -0,0 +1,13 @@
+
+const AssessmentCard = () => (
+    <div className="p-4 bg-white border rounded">
+        <h2 className="h5">Automated Assessment Scoring</h2>
+        <p>
+            Scoring is automatic, so you don't have to waste time or effort grading each answer yourself. Evalgator auto-grades
+            the answers based on predefined criteria and calculates each candidate's score.
+        </p>
+    </div>
+);
+
+export default AssessmentCard;
+   
